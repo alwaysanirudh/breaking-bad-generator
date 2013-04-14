657845acc1425b0b371ab2befdd62130f0d64107
kevin howbrook <kevinhowbrook@me.com>
2013-04-14 16:14:45 +0100

    element styles

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="elements.css">
+
+</head>
+<body>
 <?php 
   require_once("session_file.php");
     
@@ -25,8 +32,10 @@ $patterns[0] = '/a/';
 $patterns[1] = '/s/';
 $patterns[2] = '/h/';
 $replacements = array();
-$replacements[2] = 'a';
-$replacements[1] = 's';
-$replacements[0] = '<img src="images/elements/h.jpg">';
+$replacements[2] = '<div id="no-element-wrapper"><div id="no-element-inner">a</div></div>';
+$replacements[1] = '<div id="no-element-wrapper"><div id="no-element-inner">s</div></div>';
+$replacements[0] = '<div class="element-wrapper"><div class="element-top-left">1.008</div><div class="element-top-right">-1</div><div class="element-name">H</div><div class="element-bottom-one">1</div><div class="element-bottom-two">1</div></div>';
 echo preg_replace($patterns, $replacements, $string);
 ?>
+</body>
+</html>
