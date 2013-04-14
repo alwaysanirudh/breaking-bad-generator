ff4d73f55705e6e21b153a88b846afacf15c90c0
kevin howbrook <kevinhowbrook@me.com>
2013-04-14 15:41:47 +0100

    updating images and actio

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
-<form action="breakbad.php" method="get">
-Name: <input type="text" name="fname">
-<input type="submit">
+<?php 
+  require_once("session_file.php");
+    
+?>
+<strong>Test Form</strong>
+<form action="" method="post">
+<input type="text" name="name"/>
+<input type="submit" name="Submit" value="Submit!" />
 </form>
+<?php
+
+
+
+ if (isset($_POST['Submit'])) { 
+ $_SESSION['name'] = $_POST['name'];
+ } 
+
+ $break = $_SESSION['name'];
+
+?>
+<?php
+$string = "$break";
+$patterns = array();
+$patterns[0] = '/a/';
+$patterns[1] = '/s/';
+$patterns[2] = '/h/';
+$replacements = array();
+$replacements[2] = 'a';
+$replacements[1] = 's';
+$replacements[0] = '<img src="images/elements/h.jpg">';
+echo preg_replace($patterns, $replacements, $string);
+?>
---
+++ print.php
@@ -0,0 +1,8 @@
+<?php
+ // including the session file
+ require_once("session_file.php");
+ ?>
+<?php echo $_SESSION['name'];?>
+<?php
+
+?>
